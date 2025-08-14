b7d84a82b272c720e242fce839ada45a58d3f3fd
Mateusz Myalski <mateusz.myalski@gmail.com>
2025-08-14 12:19:14 +0200

    Add missing init ifle

---
+++ webtodotxt/__init__.py
@@ -0,0 +1,18 @@
+from .extensions import login_manager, users_db, app
+from .routes import bp
+
+
+def create_app(config_class=None):
+
+    if config_class:
+        app.config.from_object(config_class)
+    else:
+        app.config.from_pyfile('config.py', silent=True)
+
+    login_manager.init_app(app)
+
+    users_db.load(app.config["ACCOUNTS_DB_DIRECTORY_PATH"])
+
+    app.register_blueprint(bp)
+
+    return app
